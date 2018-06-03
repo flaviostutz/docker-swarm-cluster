2cd97fe84ca8bb6898424991dfdb554d760681b8
flaviostutz <flaviostutz@gmail.com>
2018-06-02 23:15:44 -0300

    adding metrics to script

--- initialize-services.sh
+++ initialize-services.sh
@@ -9,3 +9,6 @@ export $(cat .env) && docker stack deploy --compose-file docker-compose-ingress.
 echo "CREATING ADMINISTRATION SERVICES STACK..."
 export $(cat .env) && docker stack deploy --compose-file docker-compose-admin.yml admin
 
+echo "CREATING METRICS SERVICES STACK..."
+export $(cat .env) && docker stack deploy --compose-file docker-compose-metrics.yml metrics
+
